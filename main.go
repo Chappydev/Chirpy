afe6be1648725ee4c46eb8e4d878d8ad1a363a35
ChappyDev <danjtchapman@gmail.com>
2025-12-29 10:49:02 +0900

    Complete C1L5

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "net/http"
 
 func main() {
+  port := "8080"
+
   mux := http.NewServeMux()
-  server := &http.Server{Addr: ":8080", Handler: mux}
+  
+  // Handlers
+  mux.Handle("/", http.FileServer(http.Dir(".")))
+
+  server := &http.Server{
+    Addr: ":" + port, 
+    Handler: mux,
+  }
+
   server.ListenAndServe()
 }
